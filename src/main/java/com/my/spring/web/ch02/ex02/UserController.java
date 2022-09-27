1b857574c0f6b134758e49179b0ee7ce4e48292c
'a' <'a@a.com'>
2022-09-27 18:27:12 +0900

    0927

--- src/main/java/com/my/spring/web/ch02/ex02/UserController.java
+++ src/main/java/com/my/spring/web/ch02/ex02/UserController.java
@@ -1,9 +1,14 @@
 package com.my.spring.web.ch02.ex02;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller("ch02.ex02")
+@RequestMapping("ch02/ex02")
 public class UserController {
-
+	@GetMapping("userIn") //핸들러 선언
+	public String userIn() {
+		return "ch02/ex02/userIn"; //리턴해서 view name써줌
+	}
 }
